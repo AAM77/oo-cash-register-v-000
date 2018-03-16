f890d5c9e0770cc5a05d25650a70a36a22740399
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-03-16 10:41:55 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,3 +1,17 @@
 class CashRegister
 
+  attr_accessor 
+
+  @@items = [ ]
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+
+  end
+
+  def total
+    @total
+  end
+
 end
