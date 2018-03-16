3172f2195c28543745cebcc48ac74f494293209e
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-03-16 12:40:49 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -28,7 +28,7 @@ class CashRegister
       self.total
       "There is no discount to apply."
     else
-      @total = @total - @discount
+      @total = @total - @discount * 5
       "After the discount, the total comes to $#{@total}"
     end
   end # apply_discount
